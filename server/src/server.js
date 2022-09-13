e2f5c1c632e045b3ba0242f79a70009c5a9ca1e1
terriadsit <terriadsit@yahoo.com>
2022-09-13 16:41:40 -0500

    mongoose error loading planets

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-api:nfLd3l7iGipDWgOv@cluster0.lz04i.mongodb.net/nasa?retryWrites=true&w=majority';
+//const MONGO_URL = 'mongodb+srv://nasa-api:nfLd3l7iGipDWgOv@cluster0.lz04i.mongodb.net/nasa?retryWrites=true&w=majority';
 
 
 const http = require('http'); //node built in http module
@@ -12,26 +12,26 @@ const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
-//const { mongoConnect } = require('./services/mongo');
+const { mongoConnect } = require('./services/mongo');
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8000; // may set env in start script
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready!');
-});
+// mongoose.connection.once('open', () => {
+//     console.log('MongoDB connection ready!');
+// });
 
-mongoose.connection.on('error', (err) => {
-    console.error(err);
-});
+// mongoose.connection.on('error', (err) => {
+//     console.error(err);
+// });
 
 // load data, Mongo, before server begins responding to requests
 async function startServer() {
-    await mongoose.connect(MONGO_URL);
+    //await mongoose.connect(MONGO_URL);
 
 
-    //await mongoConnect;
+    await mongoConnect;
     await loadPlanetsData();
 
     server.listen(PORT, () => {
